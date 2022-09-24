4ddf5ee53745adaed53263ab4594cd3f09622c86
Guilherme Cardoso <cardosoghr@gmail.com>
2022-09-24 17:07:07 -0300

    adding API KEY

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-// Substitua o CHAVE_DA_API com a chave gerada no site.
+//Substitua o CHAVE_DA_API com a chave gerada no site.
 
-export const API_KEY = 'api_key=CHAVE_DA_API';
+export const API_KEY = 'api_key=bdec524e1d132a296ef34b4e0102e4b4';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
